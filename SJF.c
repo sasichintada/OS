ec5100bb9875e19f0aab8d74fc82abd84103e29b
sasichintada <sasichinthada248@gmail.com>
2025-03-28 15:14:58 +0530

    Update SJF.c

--- SJF.c
+++ SJF.c
@@ -1,28 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int bt[10], wt[10], p[10], i, k, n, temp;
-    float wavg = 0;
+int main()
+{
+    int p[20], bt[20], wt[20], tat[20], i, k, n, temp;
+    float wtavg, tatavg;
 
-    printf("Enter number of processes: ");
+    printf("\nEnter the number of processes -- ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
+    {
         p[i] = i;
-        printf("Enter burst time %d: ", i);
+        printf("Enter Burst Time for Process %d -- ", i);
         scanf("%d", &bt[i]);
     }
 
-    // Sorting burst times and process IDs using SJF (ascending order)
-    for (i = 0; i < n; i++) {
-        for (k = i + 1; k < n; k++) {
-            if (bt[i] > bt[k]) {
-                // Swap burst times
+    // Sorting according to Burst Time (SJF)
+    for(i = 0; i < n; i++)
+    {
+        for(k = i + 1; k < n; k++)
+        {
+            if(bt[i] > bt[k])
+            {
                 temp = bt[i];
                 bt[i] = bt[k];
                 bt[k] = temp;
 
-                // Swap process IDs
                 temp = p[i];
                 p[i] = p[k];
                 p[k] = temp;
@@ -30,17 +33,25 @@ int main() {
         }
     }
 
-    wt[0] = 0;
-    for (i = 1; i < n; i++) {
+    wt[0] = wtavg = 0;
+    tat[0] = tatavg = bt[0];
+
+    for(i = 1; i < n; i++)
+    {
         wt[i] = wt[i - 1] + bt[i - 1];
-        wavg += wt[i];
+        tat[i] = wt[i] + bt[i];
+        wtavg = wtavg + wt[i];
+        tatavg = tatavg + tat[i];
     }
 
-    printf("\nProcess\tBurst Time\tWaiting Time\n");
-    for (i = 0; i < n; i++) {
-        printf("P%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i]);
+    printf("\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("P%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
     }
 
-    printf("Average waiting time = %.6f\n", wavg / n);
+    printf("\nAverage Waiting Time -- %f", wtavg / n);
+    printf("\nAverage Turnaround Time -- %f\n", tatavg / n);
+
     return 0;
 }
