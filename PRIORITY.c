5f9087d9d9a67e3eaa7a51625e1d9e295421ea3b
sasichintada <sasichinthada248@gmail.com>
2025-03-28 15:16:56 +0530

    Update PRIORITY.c

--- PRIORITY.c
+++ PRIORITY.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int p[20], bt[20], pri[20], wt[20], i, k, n, temp;
-    float wavg;
+int main()
+{
+    int p[20], bt[20], pri[20], wt[20], tat[20], i, k, n, temp;
+    float wtavg, tatavg;
 
-    printf("Enter the number of processes: ");
+    printf("Enter the number of processes --- ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
+    {
         p[i] = i;
-        printf("Enter the burst time & priority of process %d: ", i);
-        scanf("%d%d", &bt[i], &pri[i]);
+        printf("Enter the Burst Time & Priority of Process %d --- ", i);
+        scanf("%d %d", &bt[i], &pri[i]);
     }
 
-    // Sorting processes based on priority (lower number = higher priority)
-    for (i = 0; i < n; i++) {
-        for (k = i + 1; k < n; k++) {
-            if (pri[i] > pri[k]) {
+    // Sort based on Priority (lower number = higher priority)
+    for(i = 0; i < n; i++)
+    {
+        for(k = i + 1; k < n; k++)
+        {
+            if(pri[i] > pri[k])
+            {
                 // Swap priority
                 temp = pri[i];
                 pri[i] = pri[k];
@@ -35,18 +40,25 @@ int main() {
         }
     }
 
-    wt[0] = wavg = 0;
+    wt[0] = wtavg = 0;
+    tat[0] = tatavg = bt[0];
 
-    for (i = 1; i < n; i++) {
+    for(i = 1; i < n; i++)
+    {
         wt[i] = wt[i - 1] + bt[i - 1];
-        wavg += wt[i];
+        tat[i] = wt[i] + bt[i];
+        wtavg = wtavg + wt[i];
+        tatavg = tatavg + tat[i];
     }
 
-    printf("\nProcess\tPriority\tBurst Time\tWaiting Time\n");
-    for (i = 0; i < n; i++) {
-        printf("P%d\t\t%d\t\t\t%d\t\t%d\n", p[i], pri[i], bt[i], wt[i]);
+    printf("\nPROCESS\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
+    for(i = 0; i < n; i++)
+    {
+        printf("\nP%d\t\t%d\t\t%d\t\t%d\t\t%d", p[i], pri[i], bt[i], wt[i], tat[i]);
     }
 
-    printf("\nAverage waiting time is %f\n", wavg / n);
+    printf("\nAverage Waiting Time is --- %f", wtavg / n);
+    printf("\nAverage Turnaround Time is --- %f\n", tatavg / n);
+
     return 0;
 }
